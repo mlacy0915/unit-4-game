d2c54c6b1fdc8e271846596bee1c2be134cfddbd
Maliah Lacy <mlacy0915@gmail.com>
2019-09-24 10:15:32 -0500

    js var html gems css gems

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,14 @@
-var 
+var win = 0;
+var lose = 0;
+var score = 0;
+var randomNum; 
+var gemNumPink;
+var gemNumPurple;
+var gemNumBlue;
+var gemNumYellow;
+
+$(document).ready(function() {
+    startGame();
+
+$("#pnkGemImg").on("click")function( {})
+}
